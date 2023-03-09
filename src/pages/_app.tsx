680fdfe68363086a5985540b1aa6291bab62661f
Alex Martin <alextravc.342@gmail.com>
2023-03-09 09:54:56 -0500

    [ADD]: initial setup + ini mui config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+
+import '@/styles/globals.css';
+import { darkTheme } from '@/themes';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+const HomePage: NextPage = () => {
+  return (
+    <Typography variant="h1" color="primary">
+      Hello
+    </Typography>
+  );
+};
+
+export default HomePage;
