6e95856fe93c0bd017dad88449c0e60e381ef7c1
Alex Martin <alextravc.342@gmail.com>
2023-03-10 09:13:23 -0500

    [ADD]: OpenJira scene

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,12 @@
 import { NextPage } from 'next';
-import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 
 import { MainLayout } from '@/layouts';
+import { OpenJira } from '@/jira';
 
 const HomePage: NextPage = () => {
   return (
     <MainLayout title="Home - OpenJira">
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={4}>
-          <Card sx={{ height: 'calc(100vh - 100px)' }}>
-            <CardHeader title="Pending" />
-
-            <CardContent>
-              {/* add new task */}
-
-              {/* list */}
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={4}>
-          <Card sx={{ height: 'calc(100vh - 100px)' }}>
-            <CardHeader title="In Progress" />
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={4}>
-          <Card sx={{ height: 'calc(100vh - 100px)' }}>
-            <CardHeader title="Completed" />
-          </Card>
-        </Grid>
-      </Grid>
+      <OpenJira />
     </MainLayout>
   );
 };
