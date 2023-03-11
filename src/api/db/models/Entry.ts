9e3a945a84ec43a8bd878a16b3eb45c511a54539
Alex Martin <alextravc.342@gmail.com>
2023-03-11 09:34:19 -0500

    [ADD]: entry model

---
+++ src/api/db/models/Entry.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, model, Model } from 'mongoose';
+
+import { Entry } from '@/interfaces';
+
+interface IEntryModel extends Entry {}
+
+const EntrySchema = new Schema({
+  description: {
+    type: String,
+    required: [true, 'Description is required!'],
+    trim: true,
+    min: 2,
+    max: 100,
+  },
+
+  // without mongo's timestamp
+  createdAt: {
+    type: Number,
+    required: [true, 'Created at is required!'],
+  },
+
+  status: {
+    type: String,
+    enum: {
+      values: ['pending', 'in-progress', 'finished'],
+      message: '{VALUE} not allowed',
+    },
+    required: [true, 'Status is required!'],
+    trim: true,
+  },
+});
+
+// export default model('Entry', EntrySchema); // node.js
+const EntryModel: Model<IEntryModel> =
+  mongoose.models.Entry || model('Entry', EntrySchema);
+
+export default EntryModel;
