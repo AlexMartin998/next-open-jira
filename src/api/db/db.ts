4d8dbcf6f045dbec7f1626957556c94a0737d949
Alex Martin <alextravc.342@gmail.com>
2023-03-11 08:45:29 -0500

    [ADD]: docker compose dev (mongo)

---
+++ src/api/db/db.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const mongoConnection = {
+  isConnected: 0,
+};
+
+export const connect = async () => {
+  if (mongoConnection.isConnected === 1) {
+    return console.log('connected');
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongoConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongoConnection.isConnected === 1) {
+      return console.log('usando last connection');
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect('');
+
+  mongoConnection.isConnected = 1;
+  console.log('conectado a mongodb');
+};
+
+export const disconnect = async () => {
+  if (mongoConnection.isConnected !== 0) return;
+
+  await mongoose.disconnect();
+  console.log('disconnected of mongodb');
+};
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,11 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
+  secret?: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  console.log(process.env);
+
+  res.status(200).json({ name: 'John Doe', secret: process.env.MONGO_URI });
 }
