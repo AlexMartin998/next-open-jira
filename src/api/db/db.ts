d72c263a01a6f64584ee8a6fe90045e6fcb14532
Alex Martin <alextravc.342@gmail.com>
2023-03-11 08:54:20 -0500

    [ADD]: connect to db

--- src/api/db/db.ts
+++ src/api/db/db.ts
@@ -6,7 +6,7 @@ const mongoConnection = {
 
 export const connect = async () => {
   if (mongoConnection.isConnected === 1) {
-    return console.log('connected');
+    return console.log('already connected');
   }
 
   if (mongoose.connections.length > 0) {
@@ -19,14 +19,14 @@ export const connect = async () => {
     await mongoose.disconnect();
   }
 
-  await mongoose.connect('');
+  await mongoose.connect(process.env.MONGO_URI || '');
 
   mongoConnection.isConnected = 1;
-  console.log('conectado a mongodb');
+  console.log('conectado a mongodb', process.env.MONGO_URI);
 };
 
 export const disconnect = async () => {
-  if (mongoConnection.isConnected !== 0) return;
+  if (mongoConnection.isConnected === 0) return;
 
   await mongoose.disconnect();
   console.log('disconnected of mongodb');
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,14 +3,11 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
   name: string;
-  secret?: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log(process.env);
-
-  res.status(200).json({ name: 'John Doe', secret: process.env.MONGO_URI });
+  res.status(200).json({ name: 'John Doe' });
 }
